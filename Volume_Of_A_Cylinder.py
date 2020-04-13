bf9dfad87d3498aa7fa406461f3e0bff150681cd
AminHoss <47458405+AminHoss@users.noreply.github.com>
2020-04-13 00:29:39 -0400

    Create Volume_Of_A_Cylinder.py

---
+++ Volume_Of_A_Cylinder.py
@@ -0,0 +1,4 @@
+radius = float(input("What is the radius of the cylinder in centimeters? "))
+height = float(input("What is the height of the cylinder in centimeters? "))
+volume = 3.14 * radius * radius * height
+print("The volume of the cylinder is {}".format(volume))
