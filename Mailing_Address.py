f59d13ef30122fcb307a382f08cf23ea45988a7c
AminHoss <47458405+AminHoss@users.noreply.github.com>
2020-04-18 00:33:09 -0400

    Create Mailing_Address.py

---
+++ Mailing_Address.py
@@ -0,0 +1,8 @@
+name = input("What is your full name? ")
+address = input("What is your street address? ")
+city = input("What is your city? ")
+state = input("What is your state (please enter abbreviated form)? ")
+zip_code = input("What is your zip code? ")
+print(name)
+print(address)
+print(city + ", " + state + " " + zip_code)
